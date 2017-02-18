b141d8c17a07ad2a5825e478ea7338ca581841fc
LaureTomas <lauretomas@gmail.com>
2017-02-18 18:56:55 +0100

    Update targets_ep.R

--- targets_ep.R
+++ targets_ep.R
@@ -1,19 +1,19 @@
 ## Copyright 2016 Maria Roman and Laureano Tomas
 
-## This file is part of CHIPPIPE
+## This file is part of MARLAU
 
-## CHIPPIE is free software: you can redistribute it and/or modify
+## MARLAU is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 
-## CHIPPIPE is distributed in the hope that it will be useful,
+## MARLAU is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 
 ## You should have received a copy of the GNU General Public License
-## along with CHIPPIPE  If not, see <http://www.gnu.org/licenses/>.
+## along with MARLAU  If not, see <http://www.gnu.org/licenses/>.
 
 args <- commandArgs(trailingOnly=TRUE)
 
